8aaa3f5582527110aef7de01fe71b31c05b13e24
James Wang <jameswang9909@hotmail.com>
2021-01-23 23:12:57 -0500

    Rework symbols layer

--- keyboards/lily58/keymaps/james9909/keymap.c
+++ keyboards/lily58/keymaps/james9909/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------| ADJUST|    |  WORK |------+------+------+------+------+------|
- * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | SYMB |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | SYMB | LGUI | LAlt | /Space  /       \Enter \  |BackSP|  FUN | RGUI |
+ *                   |      | LGUI | LAlt | /Space  /       \Enter \  |BackSP|  FUN | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,     KC_7,    KC_8,   KC_9,    KC_0, KC_MINS, \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSLS, \
    KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT, \
-  KC_LCTRL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, MO(AJS), TG(WRK),    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
-                              MO(SYM), KC_LGUI, KC_LALT,  KC_SPC,  KC_ENT, KC_BSPC, MO(FUN), KC_RGUI \
+  KC_LCTRL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, MO(AJS), TG(WRK),    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(SYM), \
+                              XXXXXXX, KC_LGUI, KC_LALT,  KC_SPC,  KC_ENT, KC_BSPC, MO(FUN),  KC_RGUI \
 ),
 /* WORKMAN
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -59,24 +59,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |   ⇩  |      |      |      |      |      |                    |      |      |      |      |      |   ⇩  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |      |
+ * |   ⇩  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |   ⇩  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |   -  |   _  |   :  |   +  |-------.    ,-------|   ~  |   =  |   .  |   {  |   }  |   |  |
+ * |   ⇩  |   [  |   ]  |   _  |   :  |   +  |-------.    ,-------|   -  |   =  |   {  |   }  |   ⇩  |   ⇩  |
  * |------+------+------+------+------+------|   ⇩   |    |   ⇩   |------+------+------+------+------+------|
- * |      |      |   `  |   <  |   >  |      |-------|    |-------|      |   \  |   /  |   [  |   ]  |      |
+ * |   ⇩  |      |      |      |      |   `  |-------|    |-------|   \  |   /  |   ⇩  |   ⇩  |   ⇩  |   ⇩  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |   ⇩  |   ⇩  |   ⇩  | /   ⇩   /       \   ⇩  \  |   ⇩  |   ⇩  |   ⇩  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [SYM] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX, \
-  XXXXXXX, XXXXXXX, KC_MINS, KC_UNDS, KC_COLN, KC_PLUS,                   KC_TILD,  KC_EQL,  KC_DOT, KC_LCBR, KC_RCBR, KC_PIPE, \
-  XXXXXXX, XXXXXXX,  KC_GRV,   KC_LT,   KC_GT, XXXXXXX, _______, _______, XXXXXXX, KC_BSLS, KC_SLSH, KC_LBRC, KC_RBRC, XXXXXXX, \
-                             _______, _______, _______, _______, _______,  _______, _______, _______\
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+  _______, KC_LBRC, KC_RBRC, KC_UNDS, KC_COLN, KC_PLUS,                   KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, _______, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD, _______, _______, KC_BSLS, KC_SLSH, _______, _______, _______, _______, \
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
 ),
 /* Fn Keys and Arrows
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -106,9 +106,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |   ⇩  |   ⇩  |   ⇩  | /   ⇩   /       \   ⇩  \  |   ⇩  |   ⇩  |   ⇩  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
