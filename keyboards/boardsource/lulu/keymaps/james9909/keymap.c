46f4fae427fc6611eec65f9ce867d5d9f98abbe5
James Wang <jameswang9909@hotmail.com>
2025-06-18 19:13:34 -0700

    Add NAV layer

--- keyboards/boardsource/lulu/keymaps/james9909/keymap.c
+++ keyboards/boardsource/lulu/keymaps/james9909/keymap.c
@@ -8,7 +8,7 @@ enum layers {
     WRK,
     SYM,
     FUN,
-    AJS,
+    NAV,
 };
 
 #define RAISE MO(_RAISE)
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|RGB_TOG|    |  WORK |------+------+------+------+------+------|
  * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | SYMB |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |ADJUST| LGUI | LAlt | /Space  /       \Enter \  |BackSP|  FUN | RGUI |
+ *                   |  FUN | LGUI | LAlt | /Space  /       \Enter \  |BackSP|  NAV | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSLS, \
    KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT, \
    KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, RGB_TOG,  TG(WRK),   KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(SYM), \
-                              MO(AJS), KC_LGUI, KC_LALT,  KC_SPC,  KC_ENT, KC_BSPC, MO(FUN),  KC_RGUI \
+                              MO(FUN), KC_LGUI, KC_LALT,  KC_SPC,   KC_ENT,KC_BSPC,  MO(NAV), KC_RGUI \
 ),
 /* WORKMAN
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -100,29 +100,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F11,  KC_F12,  _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
-/* ADJUST
+/* NAV
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |                    |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |                    |  ⇩   |  ⇩   |  Up  |  ⇩   |  ⇩   |  ⇩   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |-------.    ,-------|  ⇩   | Left | Down | Right|  ⇩   |  ⇩   |
+ * |------+------+------+------+------+------|   ⇩   |    |   ⇩   |------+------+------+------+------+------|
+ * |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |-------|    |-------|  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |  ⇩   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |  ⇩   |  ⇩   |  ⇩   | /   ⇩   /       \  ⇩   \  |  ⇩   |  ⇩   |  ⇩   |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [AJS] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  [NAV] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                   _______, _______,   KC_UP, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+                             _______, _______, _______, _______, _______, _______, _______, _______ \
   )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-   return update_tri_layer_state(state, SYM, FUN, AJS);
+   return update_tri_layer_state(state, SYM, NAV, FUN);
 }
